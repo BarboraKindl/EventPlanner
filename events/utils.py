a2656f974f80972c4dc331903f19b3eaebfbddf7
Barbora Kindlova <kindlova.barbora12@gmail.com>
2024-07-18 01:58:29 +0200

    Created a new utility module for event-related functions.

---
+++ events/utils.py
@@ -0,0 +1,10 @@
+from django.utils import timezone
+
+def is_event_upcoming(event):
+    return event.start_time > timezone.now()
+
+def get_available_resources(event):
+    return Resource.objects.exclude(event=event)
+
+def get_event_duration(event):
+    return event.end_time - event.start_time
